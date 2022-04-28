daf5cc0bd597d3845675f043aee9c9ba2c050c7e
Simon Andreas Bjørn <simonabj@protonmail.com>
2022-04-28 13:51:54 +0200

    Adds more functions to vec3

---
+++ color.c
@@ -0,0 +1,3 @@
+//
+// Created by simonabj on 4/28/22.
+//
--- vec3.c
+++ vec3.c
@@ -1,31 +1,115 @@
-//#pragma once
-
 #include <math.h>
 #include <stdlib.h>
 #include <stdio.h>
 
 struct vec3 {
-    double e0;
-    double e1;
-    double e2;
+    double x;
+    double y;
+    double z;
 };
 
 typedef struct vec3 vec3;
+typedef vec3 point;
+typedef vec3 color;
 
-vec3 vecinv(const vec3 in) {
-    return (vec3) {-in.e0, -in.e1, -in.e2};
+void printvec(FILE *restrict stream, const vec3 a) {
+    fprintf(stream, "%f %f %f", a.x, a.y, a.z);
 }
 
-char *vecformat(const vec3 in) {
-    char *buf = (char *) malloc(sizeof(char) * 100);
-    sprintf(buf, "(%.2f, %.2f, %.2f)\0", in.e0, in.e1, in.e2);
+char *formatvec(const vec3 *a, char *buf) {
+    sprintf(buf, "(%.2f, %.2f, %.2f)", a->x, a->y, a->z);
     return buf;
 }
 
-int main() {
-    vec3 a = {5, 3, 0};
+vec3 *vecinv(const vec3 *a) {
+    return &(vec3) {-a->x, -a->y, -a->z};
+}
 
-    printf("A = %s\n", vecformat(a));
-    printf("-A = %s\n", vecformat(vecinv(a)));
-    return 0;
+vec3 *addeq(vec3 *a, const vec3 *b) {
+    a->x += b->x;
+    a->y += b->y;
+    a->z += b->z;
+    return a;
+}
+
+vec3 *subeq(vec3 *a, const vec3 *b) {
+    a->x -= b->x;
+    a->y -= b->y;
+    a->z -= b->z;
+    return a;
+}
+
+vec3 *muleq(vec3 *a, const double t) {
+    a->x *= t;
+    a->y *= t;
+    a->z *= t;
+    return a;
+}
+
+vec3 *diveq(vec3 *a, const double t) {
+    return muleq(a, 1 / t);
+}
+
+vec3 *add(const vec3 *a, const vec3 *b) {
+    vec3 *v = (vec3 *) malloc(sizeof(vec3));
+    v->x = a->x + b->x;
+    v->y = a->y + b->y;
+    v->z = a->z + b->z;
+    return v;
+}
+
+vec3 *sub(const vec3 *a, const vec3 *b) {
+    vec3 *v = (vec3 *) malloc(sizeof(vec3));
+    v->x = a->x - b->x;
+    v->y = a->y - b->y;
+    v->z = a->z - b->z;
+    return v;
+}
+
+vec3 *mul(const vec3 *a, const vec3 *b) {
+    vec3 *v = (vec3 *) malloc(sizeof(vec3));
+    v->x = a->x * b->x;
+    v->y = a->y * b->y;
+    v->z = a->z * b->z;
+    return v;
+}
+
+vec3 *constmul(const vec3 *a, const double t) {
+    vec3 *v = (vec3 *) malloc(sizeof(vec3));
+    v->x = a->x * t;
+    v->y = a->y * t;
+    v->z = a->z * t;
+    return v;
+}
+
+vec3 *constdiv(const vec3 *a, const double t) {
+    vec3 *v = (vec3 *) malloc(sizeof(vec3));
+    v->x = a->x / t;
+    v->y = a->y / t;
+    v->z = a->z / t;
+    return v;
+}
+
+vec3 *cross(const vec3 *a, const vec3 *b) {
+    vec3 *v = (vec3 *) malloc(sizeof(vec3));
+    v->x = a->y * b->z - a->z * b->y;
+    v->y = a->z * b->x - a->x * b->z;
+    v->z = a->x * b->y - a->y * b->x;
+    return v;
+}
+
+double dot(const vec3 *a, const vec3 *b) {
+    return a->x * b->x + a->y * b->y + a->z * b->z;
+}
+
+double length_squared(const vec3 *a) {
+    return a->x * a->x + a->y * a->y + a->z * a->z;
+}
+
+double length(const vec3 *a) {
+    return sqrt(length_squared(a));
+}
+
+vec3 *normalize(vec3 *a) {
+    return diveq(a, length(a));
 }
